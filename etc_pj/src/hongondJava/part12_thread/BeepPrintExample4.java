a741e37abb7661b3267cf1a58b9569b528b35c21
DarkLight0418 <samseong0418@naver.com>
2026-02-09 10:48:18 +0900

    20260209 BeepPrintExample4

---
+++ etc_pj/src/hongondJava/part12_thread/BeepPrintExample4.java
@@ -0,0 +1,17 @@
+package hongondJava.part12_thread;
+
+public class BeepPrintExample4 {
+    public static void main(String[] args) {
+        Thread thread = new BeepThread();
+        thread.start();
+
+        for(int i=0; i<5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (Exception ex) {
+                System.out.println("예외 발생");
+            }
+        }
+    }
+}
