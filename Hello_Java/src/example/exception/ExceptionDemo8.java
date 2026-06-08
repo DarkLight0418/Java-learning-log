40b0c32c30e447e0a3f958bd39987229c8afc8b9
DarkLight0418 <samseong0418@naver.com>
2026-06-08 12:30:37 +0900

    [study] 2026-06-08: 자바 Exception 복습

---
+++ Hello_Java/src/example/exception/ExceptionDemo8.java
@@ -0,0 +1,15 @@
+package example;
+
+import java.io.IOException;
+
+public class ExceptionDemo8 {
+    public static void main(String[] args) {
+        System.out.print("문자를 입력하세요: ");
+        try {
+            int aChar = System.in.read();
+            System.out.println("입력한 문자 = " + (char) aChar);
+        } catch (IOException e) {
+            System.out.println("IOException 발생: " + e);
+        }
+    }
+}
---
+++ Hello_Java/src/example/exception/ExceptionDemo9.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.io.IOException;
+
+public class ExceptionDemo9 {
+    public static char readOneChar() throws IOException {
+        System.out.print("문자를 입력하세요: ");
+        return (char) System.in.read();
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("입력한 문자 = " + readOneChar());
+        } catch (IOException e) {
+            System.out.println("IOException 발생: " + e);
+        }
+    }
+}
