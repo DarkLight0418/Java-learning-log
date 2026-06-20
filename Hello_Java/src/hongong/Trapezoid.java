ecacb2b85daf335dad91ea451b316d30e12886b3
DarkLight0418 <samseong0418@naver.com>
2026-06-20 18:13:10 +0900

    [study] 2026-06-20: 혼공자바 Trapezoid 문제(2 -> 2.0 고칠 것)

--- Hello_Java/src/hongong/Trapezoid.java
+++ Hello_Java/src/hongong/Trapezoid.java
@@ -6,7 +6,7 @@ public class Trapezoid {
         int lengthBottom = 10;
         int height = 7;
 
-        double area =  (lengthTop + lengthBottom) * height / 2;
+        double area = (lengthTop + lengthBottom) * height / 2.0;
         System.out.println("Area: " + area);
     }
 }
